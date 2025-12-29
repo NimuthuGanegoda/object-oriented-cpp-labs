65d7752097d51fcbc5ac44ce2bd7bf905263b8f3
NimuthuGanegoda <nimuthuganegoda@gmail.com>
2025-12-29 15:07:05 +0530

    Add basic C++ examples for constants, data types, hello world, and variables
    - Implemented a constant example in Constant.cpp demonstrating the use of the `const` keyword.
    - Created Data Types example in Data Types.cpp showcasing various data types including int, double, char, bool, and string.
    - Added a simple hello world program in helloworld.cpp with comments.
    - Developed a variable example in variable.cpp illustrating variable declaration and assignment.

--- Youtube Guide/Constant.cpp
+++ Youtube Guide/Constant.cpp
@@ -4,14 +4,15 @@ int main(){
     // The cons keyword specifies that a variable's value is constant 
     // tells the compiler to prevent anything from modifing it 
     // (read-only)
-    double pi = 3.14;
+    const double PI = 3.14159;
     double radius = 10;
-    double circumference = 2 * pi * radius;
+    double circumference = 2 * PI * radius;
 
-    std::cout
+    std::cout << circumference << "cm";
 
-
-
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
 
     return 0;
 }
