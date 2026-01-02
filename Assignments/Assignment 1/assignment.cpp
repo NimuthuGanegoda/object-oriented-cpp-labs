6b1364c7ff9a26d54b7a77880ddf4fd22aa7914f
NimuthuGanegoda <nimuthuganegoda@gmail.com>
2026-01-02 17:47:55 +0530

    Add structure definition for Word and string conversion function

--- Assignments/Assignment 1/assignment.cpp
+++ Assignments/Assignment 1/assignment.cpp
@@ -1,4 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+#include <iomanip>
+
 using namespace std;
 
+// struct holds information for the name of the structure Word
+struct Word {
+    string name;
+    string type;
+    string definition;
+};
 
+// convert a string to lowercase
+string toLower(string s)
+{
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    return s;
+}
